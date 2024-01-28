cc909a08a5a05b1d747c7e9e4005955d05dedcc5
Александр <157222868+Skayprotask@users.noreply.github.com>
2024-01-28 23:20:22 +0400

    commit

--- str_func.py
+++ str_func.py
@@ -1,5 +1,19 @@
 def upp_text():
+    '''
+    Делает все слова в
+    тексте заглавными
+    '''
     s = input()
     upp = s.upper()
     return upp
 print(upp_text())
+
+def upp_one():
+    '''
+    Делает все слова с
+    первыми заглавными буквами
+    '''
+    t = input()
+    one = t.title()
+    return one
+print(upp_one())
